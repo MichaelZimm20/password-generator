6a8da60dd42155d3ce531f5a9b6d3ee6ca492813
DJMIKE20 <m.zimm20@gmail.com>
2022-07-26 16:44:36 -0400

    initial 8-128 character prompt

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,54 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+
+// Numeric Values array for password 0-9
+var numericValues = ['0', '1', '2', '3', '4','5','6','7','8','9'];
+
+
+//Lowercase Characters array, a-z 
+var generateLowercase = function() { 
+return [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+
+}
+// console.log(generateLowercase());
+
+//Uppercase Characters array, A-A 
+var generateUppercase = function() { 
+  return [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
+  
+  }
+  // console.log(generateUppercase());
+
+
+//Special Characters array 
+var specialChars = function() { 
+  return [...Array(15)].map((_, i) => String.fromCharCode(i + (33)));
+  
+  }
+  // console.log(specialChars());
+
+
+//The password Generator
+var passwordGenerator = function() {
+
+  //Inital How many Charcters prompt
+  var charPrompt = window.prompt('How many characters do you want you password to be? Choose between 8 - 128 charcters');
+
+  if (!charPrompt || charPrompt < 8 || charPrompt > 128){
+      window.alert("Please choose a valid number between 8 - 128.")
+
+  } else {
+    window.alert('Your password will now be '+ charPrompt + " characters long.");
+  }
+  console.log(charPrompt);
+
+
+
+
+}
+7
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +59,16 @@ function writePassword() {
 
 }
 
+
+
+
+
+
+
+var generateBtn = document.querySelector("#generate");
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+passwordGenerator();
