e3a598164e441d1d19af5f935306f132c42a7df0
DJMIKE20 <m.zimm20@gmail.com>
2022-07-26 22:47:28 -0400

    changed user input prompts to fit a while loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,69 +1,69 @@
 // Assignment Code
 // Object for confirming of Lowercase, Uppercase, Numbers, Special characters
 // // Numeric Values array for password 0-9
-// var numericValues = ['0', '1', '2', '3', '4','5','6','7','8','9'];
 
 
 
-
-
-//Lowercase Characters array, a-z 
-var generateLowercase = function() { 
-return [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
-
-}
-// console.log(generateLowercase());
-
-//Uppercase Characters array, A-A 
-var generateUppercase = function() { 
-  return [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
-  
-  }
-  // console.log(generateUppercase());
-
-
-//Special Characters array 
-var specialChars = function() { 
-  return [...Array(15)].map((_, i) => String.fromCharCode(i + (33)));
-  
-  }
-  // console.log(specialChars());
 
 
 //The password Generator
 var generatePassword = function() {
 
   //Inital How many Charcters prompt
-  var charPrompt = window.prompt('How many characters do you want you password to be? Choose between 8 - 128 charcters');
-
-  if (!charPrompt || charPrompt < 8 || charPrompt > 128) {
-    window.alert("Please choose a valid number of characters between 8 - 128.");
-      charPrompt; 
-      
-  } else {
-    
-      window.alert('Your password will now be '+ charPrompt + " characters long.");
-   
-  } 
-   
-  var confirmLowercase;
-  var confirmUppercase;
-  var confirmNum;
-  var confirmSpecialCharcaters;
-  if (charPrompt){
-    confirmLowercase = window.confirm("Would you like your password to contain lowercase characters ?"),
-    confirmUppercase = window.confirm("Would you like your password to contain uppercase characters ?"),
-    confirmNum = window.confirm("Would you like your password to contain numbers ?");
-    confirmSpecialCharcaters  = window.confirm("world you like your password to contain special characters ?");
-  } 
+  var passwordLength = window.prompt('How many characters do you want you password to be? Choose between 8 - 128 charcters');
   
-  if (!confirmLowercase && !confirmUppercase && !confirmNum && !confirmSpecialCharcaters){
-    window.alert(" Please choose at least one character!");
+  while (true) {
+    if (!passwordLength) {
+      window.alert("Not a valid number, try again!");
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please choose a valid number of characters between 8 - 128.");  
+    } else {
+      break;
+    }
   }
- 
   
+   
+  var confirmLowercase = window.confirm("Would you like your password to contain lowercase characters ?");
+  var confirmUppercase = window.confirm("Would you like your password to contain uppercase characters ?");
+  var confirmNum = window.confirm("Would you like your password to contain numbers ?");
+  var confirmSpecialCharcaters  = window.confirm("world you like your password to contain special characters ?");
+  var numericValues = ['0', '1', '2', '3', '4','5','6','7','8','9'];
+  var userOptions = [];
+
+
+  //Lowercase Characters array, a-z 
+var generateLowercase = function() { 
+  return [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+  
+  }
+  // console.log(generateLowercase());
+  
+  //Uppercase Characters array, A-A 
+  var generateUppercase = function() { 
+    return [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
+    
+    }
+    // console.log(generateUppercase());
+  
+  
+  //Special Characters array 
+  var specialChars = function() { 
+    return [...Array(15)].map((_, i) => String.fromCharCode(i + (33)));
+    
+    }
+    // console.log(specialChars());
   
 
+
+
+  // if (!confirmLowercase && !confirmUppercase && !confirmNum && !confirmSpecialCharcaters){
+  //   window.alert(" Please choose at least one character!");
+  // } else if (confirmLowercase && confirmUppercase && confirmNum && confirmSpecialCharcaters) {
+  //       userOptions = userOptions.concat(generateLowercase(), generateUppercase(), numericValues, specialChars());
+  // }
+  
+  
+//for loop for random character
 
 }
 
